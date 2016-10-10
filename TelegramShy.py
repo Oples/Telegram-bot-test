495733d9fbc1317bec1d86a9d3239a81ce6ec48e
Oples <fornasat@gmail.com>
2016-10-10 20:56:34 +0200

    Update TelegramShy.py

--- TelegramShy.py
+++ TelegramShy.py
@@ -108,7 +108,7 @@ try:
                   #bot.sendMessage (message.channel,'Bad Link')
            except:
               bot.sendMessage(chat_id=update.message.chat_id, text='Bad link :T \n\nGive me audio/video sites')
-         elif update.message.forward_from()==update.message.forward_from_chat:
+         elif update.message.chat_type=='private':
            bot.sendMessage(chat_id=update.message.chat_id, text=mind.ask(update.message.text))
            print(update.message.text)
 
