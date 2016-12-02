21018b4a3a32d7633cd6fe3170621ae0b03cb63e
Oples <fornasat@gmail.com>
2016-12-02 20:06:55 +0100

    Update run.py

--- run.py
+++ run.py
@@ -3,16 +3,15 @@ import getpass
 
 if (os.name == 'posix'):
     print('Initializing the Installation')
+    passwd = getpass.getpass('ho bisogno di essere root!\npasswd: ')
     
     if (os.system('pip3 >> /dev/null') == 0):
         print("[\033[32mok\033[0m] pip3")
-        passwd = getpass.getpass('ho bisogno di essere root per installare le librerie\npasswd: ')
         
     else:
         print('[\033[31mFail\033[0m] pip3')
         print('\ndevi installare pip3')
         print('\nScrivi: sudo apt-get install python3-pip')
-        passwd = getpass.getpass('\no inserisci la password di sudo per installarlo: ')
         print('\nInstallando ..')
         os.system('echo -e "'+passwd+'\n" | sudo -S apt-get install python3-pip')
         
