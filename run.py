e9730afb5cf9ba5a56b55e4f707c43a643402c38
Oples <fornasat@gmail.com>
2016-12-02 20:02:05 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,23 @@
 import os
-import platform 
+import getpass
 
-if (platform.system() == 'Linux'):
+if (os.name == 'posix'):
     print('Initializing the Installation')
-    if (os.system()
+    
+    if (os.system('pip3') == 0):
+        print("[\033[32mok\033[0m] pip3")
+        
+    else:
+        print('[\033[31mFail\033[0m] pip3')
+        print('\ndevi installare pip3')
+        print('\nScrivi: sudo apt-get install python3-pip')
+        passwd = getpass.getpass('\no inserisci la password di sudo per installarlo: ')
+        print('\nInstallando ..')
+        os.system('echo -e "'+passwd+'\n" | sudo -S apt-get install python3-pip')
+        
+    print('[installando le librerie]')
+    os.system('echo -e "'+passwd+'\n" | sudo -SH pip3 install Cleverbot --upgrade')
+    os.system('echo -e "'+passwd+'\n" | sudo -SH pip3 install python-telegram-bot --upgrade')
+    os.system('echo -e "'+passwd+'\n" | sudo -SH pip3 install youtube-dl --upgrade')
+    print('\nDone!')
+    print('all librarys installed now starting the bot!')
