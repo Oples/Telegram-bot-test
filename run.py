11730b64eab11126ceda0f194434fd631bca0693
Oples <fornasat@gmail.com>
2016-12-02 20:21:34 +0100

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import getpass
 
 if (os.name == 'posix'):
     print('Initializing the Installation')
-    passwd = getpass.getpass('ho bisogno di essere root!\npasswd: ')
+    passwd = getpass.getpass('ho bisogno di essere root!\npasswd:\033[5m\033[7m \033[0m')
     
     if (os.system('pip3 >> /dev/null') == 0):
         print("[\033[32mok\033[0m] pip3")
@@ -11,7 +11,7 @@ if (os.name == 'posix'):
     else:
         print('[\033[31mFail\033[0m] pip3')
         print('\ndevi installare pip3')
-        print('\nScrivi: sudo apt-get install python3-pip')
+        print('Lo installo io?')
         print('\nInstallando ..')
         os.system('''echo -e "'''+passwd+'''\n" | sudo -S apt-get install python3-pip''')
         
